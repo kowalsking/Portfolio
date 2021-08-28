e24c16ffa55de8dfe7a62e91a7f4934e44be3e30
kowalsking <kovvalsking@gmail.com>
2021-08-28 22:55:48 +0200

    Easy THREE.js setup

---
+++ app.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three'
+
+export default class Sketch {
+  constructor (options) {
+    this.container = options.domElement
+    this.height = this.container.offsetHeight
+    this.width = this.container.offsetWidth
+    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 10 )
+    this.camera.position.z = 1
+
+    this.scene = new THREE.Scene()
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setSize(this.width, this.height)
+    console.log(this.renderer.domElement)
+    this.container.append(this.renderer.domElement)
+    this.time = 0
+    this.addObjects()
+    this.render()
+  }
+
+  addObjects () {
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+    this.material = new THREE.MeshNormalMaterial()
+  
+    this.mesh = new THREE.Mesh(this.geometry, this.material)
+    this.scene.add(this.mesh)
+  }
+
+  render () {
+    this.mesh.rotation.x = this.time / 2000
+    this.mesh.rotation.y = this.time / 1000
+    this.renderer.render(this.scene, this.camera)
+    requestAnimationFrame(this.render.bind(this))
+  }
+}
+
+new Sketch({
+  domElement: document.getElementById('container')
+})
