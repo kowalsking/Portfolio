8f8e14dfa50ae5c35ff517373d5d19ecc1fcd6c9
kowalsking <kovvalsking@gmail.com>
2021-08-30 15:28:35 +0200

    Lesson 6

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ export default class Sketch {
   addObjects () {
     // this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
     this.geometry = new THREE.PlaneBufferGeometry(0.5, 0.5, 100, 100)
+    this.geometry = new THREE.SphereBufferGeometry(0.5, 30, 30)
     // this.material = new THREE.MeshNormalMaterial()
 
     this.material = new THREE.ShaderMaterial({
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,9 +1,12 @@
 uniform float time;
 varying float pulse;
 varying vec2 vUv;
+varying vec3 vNormal;
+
 
 void main() {
   vUv = uv;
+  vNormal = normal;
   vec3 newPosition = position;
   newPosition.z = 0.05 * sin(length(position) * 30. + time);
   pulse = 20. * newPosition.z;
