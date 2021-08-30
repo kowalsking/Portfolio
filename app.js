b9fc711a8d6bb46740f05120d98993ded73120c6
kowalsking <kovvalsking@gmail.com>
2021-08-30 16:52:11 +0200

    Lesson 8: Creating Zoom Effect

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
-import testTexture from './water.jpg'
+import testTexture from './texture.jpg'
+import * as dat from 'dat.gui'
 
 export default class Sketch {
   constructor (options) {
@@ -23,8 +24,17 @@ export default class Sketch {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
     this.addObjects()
+    this.setupSettings()
     this.render()
     this.setupResize()
+  }
+
+  setupSettings () {
+    this.settings = {
+      progress: 0
+    }
+    this.gui = new dat.GUI()
+    this.gui.add(this.settings, 'progress', 0, 1, 0.001)
   }
 
   resize () {
@@ -40,14 +50,13 @@ export default class Sketch {
   }
 
   addObjects () {
-    // this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
-    this.geometry = new THREE.PlaneBufferGeometry(350, 350, 100, 100)
-    // this.material = new THREE.MeshNormalMaterial()
+    this.geometry = new THREE.PlaneBufferGeometry(300, 300, 100, 100)
 
     this.material = new THREE.ShaderMaterial({
       wireframe: false,
       uniforms: {
         time: { value: 1.0 },
+        uProgress: { value: 1.0 },
         uTexture: { value: new THREE.TextureLoader().load(testTexture) },
         resolution: { value: new THREE.Vector2() }
       },
@@ -57,11 +66,15 @@ export default class Sketch {
   
     this.mesh = new THREE.Mesh(this.geometry, this.material)
     this.scene.add(this.mesh)
+
+    this.mesh.position.x = 300
+    this.mesh.rotation.z = 0.5
   }
 
   render () {
     this.time += 0.05
     this.material.uniforms.time.value = this.time
+    this.material.uniforms.uProgress.value = this.settings.progress
     this.mesh.rotation.x = this.time / 2000
     this.mesh.rotation.y = this.time / 1000
     this.renderer.render(this.scene, this.camera)
