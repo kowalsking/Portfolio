7e421747ef43a6b941500a877a6ce7f4176929c8
kowalsking <kovvalsking@gmail.com>
2021-08-31 16:53:10 +0200

    Lesson 11: Effect with corners

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 import testTexture from './texture.jpg'
 import * as dat from 'dat.gui'
+import gsap from 'gsap'
 
 export default class Sketch {
   constructor (options) {
@@ -56,15 +57,34 @@ export default class Sketch {
       wireframe: false,
       uniforms: {
         time: { value: 1.0 },
-        uProgress: { value: 1.0 },
+        uProgress: { value: 0 },
         uTexture: { value: new THREE.TextureLoader().load(testTexture) },
         uResolution: { value: new THREE.Vector2(this.width, this.height) },
         uQuadSize: { value: new THREE.Vector2(300, 300) },
-        uTextureSize: { value: new THREE.Vector2(100, 100) }
+        uTextureSize: { value: new THREE.Vector2(100, 100) },
+        uCorners: { value: new THREE.Vector4(0, 0, 0, 0) }
       },
       vertexShader: vertex,
       fragmentShader: fragment
     })
+
+    this.tl = gsap.timeline()
+      .to(this.material.uniforms.uCorners.value, {
+        x: 1,
+        duration: 1
+      })
+      .to(this.material.uniforms.uCorners.value, {
+        y: 1,
+        duration: 1
+      }, 0.1)
+      .to(this.material.uniforms.uCorners.value, {
+        z: 1,
+        duration: 1
+      }, 0.2)
+      .to(this.material.uniforms.uCorners.value, {
+        w: 1,
+        duration: 1
+      }, 0.3)
   
     this.mesh = new THREE.Mesh(this.geometry, this.material)
     this.scene.add(this.mesh)
@@ -77,7 +97,8 @@ export default class Sketch {
   render () {
     this.time += 0.05
     this.material.uniforms.time.value = this.time
-    this.material.uniforms.uProgress.value = this.settings.progress
+    // this.material.uniforms.uProgress.value = this.settings.progress
+    this.tl.progress(this.settings.progress)
     this.mesh.rotation.x = this.time / 2000
     this.mesh.rotation.y = this.time / 1000
     this.renderer.render(this.scene, this.camera)
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -2,6 +2,7 @@ uniform float time;
 uniform float uProgress;
 uniform vec2 uResolution;
 uniform vec2 uQuadSize;
+uniform vec4 uCorners;
 varying vec2  vSize;
 
 varying vec2 vUv;
@@ -13,7 +14,13 @@ void main() {
   fullScreenState.x *= uResolution.x / uQuadSize.x;
   fullScreenState.y *= uResolution.y / uQuadSize.y;
 
-  vec4 finalState = mix(defaultState, fullScreenState, uProgress);
+  float cornersProgress = mix(
+    mix(uCorners.x, uCorners.y, uv.x),
+    mix(uCorners.z, uCorners.w, uv.x),
+    uv.y
+  );
+
+  vec4 finalState = mix(defaultState, fullScreenState, cornersProgress);
 
   vSize = mix(uQuadSize, uResolution, uProgress);
 
